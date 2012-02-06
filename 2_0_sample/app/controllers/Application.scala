4d37cf10208f14337dbe2511ab90964772cd8794
RKTM <rrrkitamura@gmail.com>
2012-02-07 02:08:56 +0900

    write sample hello world application.

--- 2_0_sample/app/controllers/Application.scala
+++ 2_0_sample/app/controllers/Application.scala
@@ -2,11 +2,43 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+
+import views._
+
 
 object Application extends Controller {
   
+   /**
+   * Describes the hello form.
+   */
+  val helloForm = Form(
+    of(
+      "name" -> requiredText,
+      "repeat" -> number(min = 1, max = 100),
+      "color" -> optional(text)
+    )
+  )
+  
+   /**
+   * Home page
+   */
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(html.input(helloForm))
   }
   
+//  def index = Action {
+//    Ok(views.html.index("Your new application is ready!"))
+//  }
+
+  /**
+   * Handles the form submission.
+   */
+  def sayHello = Action { implicit request =>
+    helloForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(html.input(formWithErrors)),
+      {case (name, repeat, color) => Ok(html.hello(name, repeat.toInt, color))}
+    )
+  }
+ 
 }
